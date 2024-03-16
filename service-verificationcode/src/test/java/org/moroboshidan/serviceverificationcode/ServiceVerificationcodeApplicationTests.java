3a842a7b1e975bacf0ff1c2a454622cbc2799127
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-16 19:47:19 +0800

    创建了service-verificationcode模块，并注册了nacos

---
+++ service-verificationcode/src/test/java/org/moroboshidan/serviceverificationcode/ServiceVerificationcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moroboshidan.serviceverificationcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceVerificationcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6ab042c89ea098d775f2e2fa4d8dfbc1293ffbfb
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-16 17:51:55 +0800

    api-passenger项目基础骨架

---
+++ api-passenger/src/main/java/org/moroboshidan/ApiPassengerApplication.java
@@ -0,0 +1,17 @@
+package org.moroboshidan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ApiPassengerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ApiPassengerApplication.class, args);
+    }
+}

20b62d6c2de5717153dd61dcc0c341f7941a4c1b
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-17 20:43:38 +0800

    api-passenger发送验证码服务完成，存入redis缓存

--- api-passenger/src/main/java/org/moroboshidan/service/VerificationCodeService.java
+++ api-passenger/src/main/java/org/moroboshidan/service/VerificationCodeService.java
@@ -4,12 +4,19 @@ import org.moroboshidan.internalcommon.dto.ResponseResult;
 import org.moroboshidan.internalcommon.response.NumberCodeResponse;
 import org.moroboshidan.remote.ServiceVerificationClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class VerificationCodeService {
     @Autowired
     private ServiceVerificationClient serviceVerificationClient;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private String verificationCodePrefix = "passenger-verification";
 
     public ResponseResult generateCode(String passengerPhone) {
         // 调用验证码服务，获取验证码
@@ -17,11 +24,14 @@ public class VerificationCodeService {
         ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationClient.getNumberCode(6);
         int numberCode = numberCodeResponse.getData().getNumberCode();
         System.out.println("remote number code: " +numberCode);
-
         // 存入redis
+        // 需要 key, value
+        String key = verificationCodePrefix + passengerPhone;
+        stringRedisTemplate.opsForValue().set(key, numberCode + "", 2, TimeUnit.MINUTES);
         System.out.println("将验证码存入redis");
-        // 返回结果
+        // 通过短信服务，将验证码发送至对应的手机上
 
+        // 返回结果
         return ResponseResult.success();
     }
 }

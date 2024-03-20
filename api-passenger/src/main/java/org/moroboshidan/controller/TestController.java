0e8224081bfc78d93486480daa9bb30f6f98dfb3
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-20 15:24:11 +0800

    测试token的controller方法

--- api-passenger/src/main/java/org/moroboshidan/controller/TestController.java
+++ api-passenger/src/main/java/org/moroboshidan/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.moroboshidan.controller;
 
+import org.moroboshidan.internalcommon.dto.ResponseResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,14 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "test api passenger";
+    }
+
+    @GetMapping("/authTestNoToken")
+    public ResponseResult authTest() {
+        return ResponseResult.success("auth test without token");
+    }
+    @GetMapping("/authTest")
+    public ResponseResult authTestWithToken() {
+        return ResponseResult.success("auth test with token");
     }
 }

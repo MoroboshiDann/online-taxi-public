f73f096ebff0c289ce26b940bbe00dc2398e4ce0
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-21 16:23:43 +0800

    service-map项目创建

--- api-passenger/src/main/java/org/moroboshidan/service/ForecastPriceService.java
+++ api-passenger/src/main/java/org/moroboshidan/service/ForecastPriceService.java
@@ -1,22 +1,26 @@
 package org.moroboshidan.service;
 
 import org.moroboshidan.internalcommon.dto.ResponseResult;
+import org.moroboshidan.internalcommon.request.ForecastPriceDTO;
 import org.moroboshidan.internalcommon.response.ForecastPriceResponse;
+import org.moroboshidan.remote.ServicePriceClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ForecastPriceService {
+    @Autowired
+    private ServicePriceClient servicePriceClient;
 
     /**
      * 根据出发地和目的地的经纬度计算预估价格
+     *
      * @param
      * @return
      * @throws
-     *
      */
     public ResponseResult forecastPrice(String depLongitude, String depLatitude, String destLongitude, String destLatitude) {
-        ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
-        forecastPriceResponse.setPrice(3.14);
-        return ResponseResult.success(forecastPriceResponse);
+        ResponseResult responseResult = servicePriceClient.forecastPrice(new ForecastPriceDTO(depLongitude, depLatitude, destLongitude, destLatitude));
+        return ResponseResult.success(responseResult.getData());
     }
 }

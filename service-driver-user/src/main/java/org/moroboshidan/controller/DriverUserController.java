bb6fddd7b442ea9b6648816ce8f13d15395b145d
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-22 21:25:15 +0800

    service-driver-user修改司机信息

--- service-driver-user/src/main/java/org/moroboshidan/controller/DriverUserController.java
+++ service-driver-user/src/main/java/org/moroboshidan/controller/DriverUserController.java
@@ -4,10 +4,7 @@ import org.moroboshidan.internalcommon.dto.DriverUser;
 import org.moroboshidan.internalcommon.dto.ResponseResult;
 import org.moroboshidan.service.DriverUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DriverUserController {
@@ -18,9 +15,29 @@ public class DriverUserController {
         return driverUserService.tetGetDriverUser();
     }
 
+    /**
+     * 新增司机信息
+     * @param
+     * @return
+     * @throws
+     *
+     */
     @PostMapping("/user")
     public ResponseResult addUser(@RequestBody DriverUser driverUser) {
         System.out.println(driverUser.toString());
         return driverUserService.addDriverUser(driverUser);
     }
+
+    /**
+     * 修改司机信息
+     * @param
+     * @return
+     * @throws
+     *
+     */
+    @PutMapping("/user")
+    public ResponseResult updateUser(@RequestBody DriverUser driverUser) {
+        return driverUserService.updateDriverUser(driverUser);
+    }
+
 }

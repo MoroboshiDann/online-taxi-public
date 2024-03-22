278079d24030893cf751121b49204c636c9a5896
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-22 18:45:24 +0800

    修改日期格式

--- internal-common/src/main/java/org/moroboshidan/internalcommon/dto/DriverUser.java
+++ internal-common/src/main/java/org/moroboshidan/internalcommon/dto/DriverUser.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -14,25 +14,25 @@ public class DriverUser {
     private String address;
     private String driverName;
     private String driverPhone;
-    private Date driverBirthday;
+    private LocalDateTime driverBirthday;
     private String driverNation;
     private String driverContactAddress;
     private String licenseId;
-    private Date getDriverLicenseDate;
-    private Date driverLicenseOn;
-    private Date driverLicenseOff;
+    private LocalDateTime getDriverLicenseDate;
+    private LocalDateTime driverLicenseOn;
+    private LocalDateTime driverLicenseOff;
     private Integer taxiDriver;
     private String certificateNo;
     private String networkCarIssueOrganization;
-    private Date networkCarIssueDate;
-    private Date getNetworkCarProofDate;
-    private Date networkCarProofOn;
-    private Date networkCarProofOff;
+    private LocalDateTime networkCarIssueDate;
+    private LocalDateTime getNetworkCarProofDate;
+    private LocalDateTime networkCarProofOn;
+    private LocalDateTime networkCarProofOff;
     private Integer commercialType;
     private String contractCompany;
-    private Date contractOn;
-    private Date contractOff;
+    private LocalDateTime contractOn;
+    private LocalDateTime contractOff;
     private Integer state;
-    private Date gmtCreate;
-    private Date gmtUpdate;
+    private LocalDateTime gmtCreate;
+    private LocalDateTime gmtUpdate;
 }

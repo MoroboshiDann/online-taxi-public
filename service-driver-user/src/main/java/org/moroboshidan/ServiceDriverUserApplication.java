06560092735371fdcff1de0b23e76b9f2b9f7a77
MoroboshiDan <hi.jqzhang@gmail.com>
2024-03-22 19:28:17 +0800

    service-driver-user注册到nacos。(注：spring cloud自edgware之后，启动类上的@EnableDiscoveryClient就可以省略了，只要做好配置就可自动注册)

--- service-driver-user/src/main/java/org/moroboshidan/ServiceDriverUserApplication.java
+++ service-driver-user/src/main/java/org/moroboshidan/ServiceDriverUserApplication.java
@@ -3,6 +3,7 @@ package org.moroboshidan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("org.moroboshidan.mapper")
+@EnableDiscoveryClient
 public class ServiceDriverUserApplication
 {
     public static void main( String[] args )
--- service-map/src/main/java/org/moroboshidan/ServiceMapApplication.java
+++ service-map/src/main/java/org/moroboshidan/ServiceMapApplication.java
@@ -3,6 +3,7 @@ package org.moroboshidan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @MapperScan("org.moroboshidan.mapper")
+@EnableDiscoveryClient
 public class ServiceMapApplication
 {
     public static void main( String[] args )
